db9190dd8a6da289c5837d5846679caffd22c64c
Moiz Qureshi <moizq@calpak-usa.com>
2016-11-12 22:34:36 -0800

    Added dummy code to test make file. Works.

---
+++ Beeth9Asm.cpp
@@ -0,0 +1,22 @@
+/*
+ * Filename: Beeth9Asm.cpp
+ * Authors: Moiz Qureshi, Ye Huang, Eduardo Rosales
+ * Date 11/9/16
+ * Description: Driver for Beeth 9 Assembler. Converts Beeth9 assembly code 
+ *              into Beeth9 ISA machine code (9 bit instructions)
+ */
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main (int argc, char **argv) {
+
+  cout << "Hello World" << endl;
+
+  return 0;
+
+}
