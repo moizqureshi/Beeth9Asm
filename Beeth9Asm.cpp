5a5d2aea24d1544efea018264a66378593a34fc9
Moiz Qureshi <moizq@calpak-usa.com>
2016-11-13 00:18:39 -0800

    Added file checking error logic and correct number of arguments

--- Beeth9Asm.cpp
+++ Beeth9Asm.cpp
@@ -1,5 +1,4 @@
-/*
- * Filename: Beeth9Asm.cpp
+/* Filename: Beeth9Asm.cpp
  * Authors: Moiz Qureshi, Ye Huang, Eduardo Rosales
  * Date 11/9/16
  * Description: Driver for Beeth 9 Assembler. Converts Beeth9 assembly code 
@@ -12,6 +11,8 @@
 
 using namespace std;
 
+// checkFilenameErrors Function Signature
+bool checkFilenameErrors(int argc, char **argv);
 
 int main (int argc, char **argv) {
 
@@ -20,3 +21,35 @@ int main (int argc, char **argv) {
   return 0;
 
 }
+
+/* Function Name: checkFilenameErrors()
+ * Function Prototype: checkFilenameErrors(int argc, char **argv)
+ * Description: Function that checks to see if number of input arguments to 
+ *              the Beeth9 Assembler program is correct and validates input
+ *              and output filenames
+ * Input Parameters: 
+ *    arg1 - int argc
+ *    arg2 - char **argv
+ * Return Values/Type: boolean - true if there were no errors, otehrwise false
+ */
+bool checkFilenameErrors(int argc, char **argv) {
+  // Check if number of arguments is 3, if not then output error message and
+  // return true 
+  if (argc != 3) {
+    cout << "Incorrect number of arguments!" << endl;
+    cout << "Please run Beeth9 Assembler as following:" << endl;
+    cout << "./Beeth9Asm infile outfile" << endl;
+    return true;
+  }
+  // Check if input and output filenames are not the same, if they are then
+  // output error message and return true
+  else if (strcmp(argv[1], argv[2]) == 0) {
+    cout << "The input and output files cannot be the same!" << endl;
+    cout << "Please try again with correct inputs." << endl;
+    return true;
+  } else {
+    // Return false since there were errors we reached this point
+    return false;
+  }
+}
+
