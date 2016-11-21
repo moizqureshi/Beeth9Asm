bb1fe9a458a9b43e5647810df3c9625d40e1565a
Moiz Qureshi <moizq@calpak-usa.com>
2016-11-20 23:14:33 -0800

    Fixed error of misspelled assembly mnemonic

--- Problem18.s
+++ Problem18.s
@@ -14,9 +14,9 @@
 
 /* Registers Used:
  * r0 - temporary register
- * r1 - bit pattern loaded from mem address 6
+ * r1 - bit pattern loaded from mem address 6 
  * r2 - temporary array byte element holder
- * r3 - start mem address of array (32) and increments to get next byte
+ * r3 - start mem address of array (32) and increments to get next byte 
  * 	offset
  * r4 - loop counter
  * r5 - loop limit
@@ -28,51 +28,49 @@
 
 	li 6			# load value 6 into r0 (mem address of pattern)
 	add $r1, $r0		# copy r0 into r1
-	lw ($r1)		# load pattern byte into r1
+	lw ($r1)		# load pattern byte into r1 
 
 	li 32			# load value 32 into r0 (mem address of array)
 	add $r3, $r0		# copy r0 into r3
 
 
-	li 63			# load value 63  into r0
+	li 63			# load value 63  into r0 
 	add $r5, $r0		# copy r0 into r5
 	inc $r5			# increment r5 by 1 (outerloop counter) to get
 				# value of 64
-
-	add $r5, $r5, $r3 	# add 32 to r5, so limit will be at 96'th byte 
-				# location in data memory
+	
+	add $r5, $r3		# Add 32 to 64, to get outerloop limit
 
 	slt $r4, $r5		# rOv = (r4 < r5) ? 1:0
-
-	li 13			# load value of 12 (+offset to end)
+	
+	li 13			# load value of 13 (+offset to end)
 	bez $r0			# if rOv == 0, then branch to end
-
+	
 	clr $r2			# clear r2
 	add $r2, $r3		# r2 = r2 + r3
 	lw ($r2)		# load next byte value into r2
 
 	patr $r2		# checks the five locations of register r2 byte
-				# and determines if it contains the 4 bit
+				# and determines if it contains the 4 bit 
 				# pattern in the LSBs of register r1 (r1[3:0]),
 				# if so then rOv is set to 1
 				# rOv = (r2 contains r1[3:0]) ? 1:0
-
-	li 2			# load value of 1 (+offset to loopEnd)
+	
+	li 2			# load value of 2 (+offset to loopEnd)
 	bez $r0			# if rOv == 0, then branch to loopEnd
-
-	inc $r7			# increment r7 (pattern count), since 4 bit
+	
+	inc $r7			# increment r7 (pattern count), since 4 bit 
 				# pattern was found at least once in the byte
-
 
 	inc $r3			# increment r3 (offset to get next byte)
 	inc $r4			# increment r4 (loop counter)
-
-	li 14 			# load value of into r0
-	twcmp $r0		# take 2's complement of 13 to get -13 in r0
+	
+	li 14			# load value of 14 into r0
+	twcmp $r0		# take 2's complement of 14 to get -14 in r0
 	branch $r0		# always branch to loop
 
 	li 7			# load value of 7 into r0
 	sw $r7			# store r7 (pattern count) into mem address
 				# pointed by r0 (mem address 7)
-
+	
 	halt 			# send halt signal to end program
